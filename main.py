246a1e261e80a695ec0f8d8bac6e179ea0cf6b65
mm911mm <mm911mm@my.com>
2025-07-15 10:04:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 from telegram.ext import Updater, CommandHandler
 
-# دالة الاستجابة لأمر /start
 def start(update, context):
     update.message.reply_text("مرحبًا بك في بوت 1M Journey 🚀")
 
-# الدالة الرئيسية لتشغيل البوت
 def main():
-    # ضع التوكن الخاص بك هنا أو اجعله يُقرأ من environment variable
     import os
     TOKEN = os.getenv("BOT_TOKEN")
 
     updater = Updater(token=TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    # إضافة أمر /start
     dp.add_handler(CommandHandler("start", start))
 
-    # بدء تشغيل البوت
     updater.start_polling()
     updater.idle()
 
